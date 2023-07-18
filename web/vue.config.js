44874b184754bd0a2f494053b0c1153474c9e5c4
binbinly <binbinly@hotmail.com>
2023-07-18 15:02:58 +0800

    edit

--- web/vue.config.js
+++ web/vue.config.js
@@ -40,7 +40,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './', // 署应用包时的基本 URL。 vue-router hash 模式使用
   //  publicPath: '/app/', //署应用包时的基本 URL。  vue-router history模式使用
-  outputDir: '../../chat-frontend/docs', //  生产环境构建文件的目录
+  outputDir: 'dist', //  生产环境构建文件的目录
   assetsDir: 'static', //  outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: !IS_PROD,
   productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
